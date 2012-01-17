f0dd1b93e865125efc2188d0f4757be8c1dc8457
Dhananjay Nene <dhananjay.nene@gmail.com>
2012-01-17 09:54:20 +0530

    Added dependency on logging libraries and added log statement

--- src/main/scala/in/vayana/blog/gradle_demo/GradleDemo.scala
+++ src/main/scala/in/vayana/blog/gradle_demo/GradleDemo.scala
@@ -1,7 +1,10 @@
 package in.vayana.blog.gradle_demo
+import com.weiglewilczek.slf4s.Logger
+
 
 object GradleDemo {
-	def main(args: Array[String]) = {
-	  println("hello world!")
-	}
+  val logger = Logger("in.vayana.blog.gradle_demo")
+  def main(args: Array[String]) = {
+	logger.debug("hello world!")
+  }
 }
